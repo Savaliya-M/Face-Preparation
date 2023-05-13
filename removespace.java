ea75ea15fa51256f9576c1a3925705a537ba97ef
Savaliya-M <masavaliya183@gmail.com>
2023-05-13 21:27:21 +0530

    Added remove space algorithm

---
+++ removespace.java
@@ -0,0 +1,10 @@
+public class string {
+
+    public static void main(String[] args) {
+        String str = "abc jf fh fdsfd efedv";
+        String nstsr = str.replaceAll("\\s", "");
+        int i = 0;
+        while(i)
+    }
+
+}
