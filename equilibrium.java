fb4e2b2d5695f5db298b12206f5e76683a41589e
Savaliya-M <masavaliya183@gmail.com>
2023-05-15 23:11:51 +0530

    Added equilibirum  program

--- equilibrium.java
+++ equilibrium.java
@@ -2,7 +2,7 @@ public class equilibrium {
     public static void main(String[] args) {
         // int[] arr = new int[] { -2, 5, 3, 1, 2, 6, -4, 2 };
         // int[] arr = new int[] { -1, 2, 3, 0, 3, 2, -1 };
-        int[] arr = new int[] { -1, 2, 3, 0, 0, 3, 2, -1 };
+        int[] arr = new int[] { -1, 26, -2, 1, -22, 6, 17, 23 };
 
         int osum = 0;
         for (int i = 0; i < arr.length; i++) {
@@ -13,6 +13,7 @@ public class equilibrium {
             osum += arr[i];
             if (osum == isum) {
                 System.out.println("point " + i);
+                System.out.println(osum);
                 break;
             }
         }
